84eea2e1059200e742981878f1f58eebb3acd309
atkhamov <atkhamov@gmail.com>
2020-05-24 02:52:41 +0600

    carousel working, products section is done

--- js/plugins.js
+++ js/plugins.js
@@ -25,7 +25,7 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     $('#navbarResponsive').removeClass('show');
 
     setTimeout(function () {
-        $('nav.navbar').removeClass('solid-toggle');    
+        $('nav.navbar').removeClass('solid-toggle');
     }, 300);
 
     
@@ -33,16 +33,41 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
-})
+});
 
 /*========== BOUNCING DOWN ARROW ==========*/
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
+    });
+});
 
 
 /*========== LIGHTBOX IMAGE GALLERY ==========*/
 
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-
+$(document).ready(function(){ //when document(DOM) loads completely
+    $('#team-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //768px width and up display 2 items
+            },
+            992 : {
+                items: 3 //992px width and up display 3 items
+            }
+        }
+    });
+});
 
 /*========== SKILLS COUNTER ==========*/
 
@@ -86,51 +111,51 @@ $(function () { // a self calling function
     onScrollInit($('.os-animation')); //function call with only items
     onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
   });
-
-
-/*========== CONTACT FORM INPUT VALIDATION ==========*/
-//Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
-$(function () {
-
-  // init the validator
-  // validator files are included in the download package
-  // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-  $('#contact-form').validator();
-
-
-  // when the form is submitted
-  $('#contact-form').on('submit', function (e) {
-
-      // if the validator does not prevent form submit
-      if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php";
-
-          // POST values in the background the the script URL
-          $.ajax({
-              type: "POST",
-              url: url,
-              data: $(this).serialize(),
-              success: function (data) {
-                  // data = JSON object that contact.php returns
-
-                  // we recieve the type of the message: success x danger and apply it to the
-                  var messageAlert = 'alert-' + data.type;
-                  var messageText = data.message;
-
-                  // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-
-                  // If we have messageAlert and messageText
-                  if (messageAlert && messageText) {
-                      // inject the alert to .messages div in our form
-                      $('#contact-form').find('.messages').html(alertBox);
-                      // empty the form
-                      $('#contact-form')[0].reset();
+  
+  
+  /*========== CONTACT FORM INPUT VALIDATION ==========*/
+  //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
+  $(function () {
+  
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+  
+    $('#contact-form').validator();
+  
+  
+    // when the form is submitted
+      $('#contact-form').on('submit', function (e) {
+  
+          // if the validator does not prevent form submit
+          if (!e.isDefaultPrevented()) {
+              var url = "contact/contact.php";
+  
+              // POST values in the background the the script URL
+              $.ajax({
+                  type: "POST",
+                  url: url,
+                  data: $(this).serialize(),
+                  success: function (data) {
+                      // data = JSON object that contact.php returns
+  
+                      // we recieve the type of the message: success x danger and apply it to the
+                      var messageAlert = 'alert-' + data.type;
+                      var messageText = data.message;
+  
+                      // let's compose Bootstrap alert box HTML
+                      var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+  
+                      // If we have messageAlert and messageText
+                      if (messageAlert && messageText) {
+                          // inject the alert to .messages div in our form
+                          $('#contact-form').find('.messages').html(alertBox);
+                          // empty the form
+                          $('#contact-form')[0].reset();
+                      }
                   }
-              }
-          });
-          return false;
-      }
-  })
-});
+              });
+              return false;
+          }
+      });
+  });
